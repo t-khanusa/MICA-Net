d16c7bb53caea716bf88ceb4ab11f6121e04d995
Nguyen Thai Khanh <102515240+t-khanusa@users.noreply.github.com>
2025-11-20 02:20:54 +0700

    Replace alpha with learnable parameter in MICA_Net

--- model/MICA_Net.py
+++ model/MICA_Net.py
@@ -6,7 +6,6 @@ from torch import nn
 from torch.nn import functional as F
 import sys
 
-alpha = 0.8
 class MICA_Net(nn.Module):
     def __init__(self):
         super(MICA_Net, self).__init__()
@@ -30,6 +29,7 @@ class MICA_Net(nn.Module):
         self.relu = nn.ReLU()
         self.regressor = nn.Sequential(nn.Linear(1024, 128), nn.Dropout(0.6), nn.Linear(128, 32))
         # self.regressor = nn.Sequential(nn.Dropout(0.6), nn.Linear(1024, 12))
+        self.alpha = nn.Parameter(torch.tensor(0.5))
 
 
     def forward(self, f1_norm, f2_norm):
@@ -50,11 +50,11 @@ class MICA_Net(nn.Module):
             v_t = self.affine_v(imu_vis_fts.transpose(0, 1))
             att_vis = torch.mm(vis_fts, v_t)
             vis_att = self.tanh(torch.div(att_vis, math.sqrt(imu_vis_fts.shape[1])))
-            H_a = self.relu(alpha *(self.W_ca(imu_att) +self.W_a(imu_fts)))
-            H_v = self.relu((1-alpha) *(self.W_cv(vis_att) + self.W_v(vis_fts)))
+            H_a = self.relu(self.alpha *(self.W_ca(imu_att) +self.W_a(imu_fts)))
+            H_v = self.relu((1-self.alpha) *(self.W_cv(vis_att) + self.W_v(vis_fts)))
 
-            att_imu_features = alpha * self.W_ha(H_a).transpose(0, 1) + imu_fts
-            att_visual_features = (1-alpha) * self.W_hv(H_v).transpose(0, 1) + vis_fts
+            att_imu_features = self.alpha * self.W_ha(H_a).transpose(0, 1) + imu_fts
+            att_visual_features = (1-self.alpha) * self.W_hv(H_v).transpose(0, 1) + vis_fts
 
             imu_visualfeatures = torch.cat((att_imu_features, att_visual_features), 1)
             imu_visualfeatures = torch.flatten(imu_visualfeatures)
